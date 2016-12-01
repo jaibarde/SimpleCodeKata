58f93f9fc3590673f3c9dd35d45ffcd075757fea
Barde <jaideep.barde@siemens.com>
2016-12-01 14:26:19 +0530

    Newly added

--- SimpleCodeKata/Program.cs
+++ SimpleCodeKata/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
@@ -18,18 +15,7 @@ namespace ConsoleApplication1
             lruObj1.Add("a");
             lruObj1.Add("c");
             Console.WriteLine("Implementation #1 : " + lruObj1.Output() + " Count : " + lruObj1.Count);
-            
             Console.WriteLine("Element @ #2 : " + lruObj1[2]);
-
-
-            LeastRecentlyUsedString lruObj2 = new LeastRecentlyUsedString();
-            lruObj2.Add("a");
-            lruObj2.Add("b");
-            lruObj2.Add("c");
-            lruObj2.Add("a");
-            lruObj2.Add("c");
-            Console.WriteLine("Implementation #2 : " + lruObj2.Output() + " Count : " + lruObj2.Count);
-            Console.WriteLine("Element @ #2 : " + lruObj2[2]);
         }
     }
 
--- SimpleCodeKata/Properties/AssemblyInfo.cs
+++ SimpleCodeKata/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("SimpleCodeKata")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Siemens")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("SimpleCodeKata")]
 [assembly: AssemblyCopyright("Copyright © Siemens 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
